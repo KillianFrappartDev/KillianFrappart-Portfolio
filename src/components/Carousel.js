435c243f427e7101fdac2c6d6debf471f8feb5fb
KillianFrappartDev <KillianFrappartDev@gmail.com>
2020-10-01 18:55:11 +0200

    hover effect

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import carouselStyles from '../styles/Carousel.module.scss';
 
 const Carousel = (props) => {
+  const [current, setCurrent] = useState(0);
+
+  const prev = () => {
+    if (current !== 0) {
+      setCurrent(current - 1);
+    } else {
+      setCurrent(props.items.length - 1);
+    }
+  };
+
+  const next = () => {
+    if (current < props.items.length - 1) {
+      setCurrent(current + 1);
+    } else {
+      setCurrent(0);
+    }
+  };
+
   return (
     <div className={carouselStyles.container}>
-      <a className={carouselStyles.icon}>
+      <a onClick={prev} className={carouselStyles.icon}>
         <i className="fas fa-chevron-left"></i>
       </a>
       <div className={carouselStyles.wrapper}>
-        <img className={carouselStyles.image} src="project-1.png" />
-        <div className={carouselStyles.left} />
-        <div className={carouselStyles.right} />
+        <img
+          className={carouselStyles.image}
+          src={props.items[current].imgURL}
+        />
+        <div className={carouselStyles.left}>
+          <a className={carouselStyles.link}>
+            <i className="fas fa-play"></i>
+          </a>
+        </div>
+        <div className={carouselStyles.right}>
+          <a className={carouselStyles.link}>
+            <i className="fas fa-code"></i>
+          </a>
+        </div>
       </div>
-      <a className={carouselStyles.icon}>
+      <a onClick={next} className={carouselStyles.icon}>
         <i className="fas fa-chevron-right"></i>
       </a>
     </div>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,10 +3,15 @@ import React from 'react';
 import Carousel from '../components/Carousel';
 import Background from '../components/Background';
 
+const PROJECTS = [
+  { title: 'Chat Group', imgURL: 'project-1.png' },
+  { title: 'ADN Clone', imgURL: 'project-2.png' },
+];
+
 const projects = () => {
   return (
     <Background>
-      <Carousel />
+      <Carousel items={PROJECTS} />
     </Background>
   );
 };
