339783b967b9fba7070ae4e49620e236230e1b36
KillianFrappartDev <KillianFrappartDev@gmail.com>
2020-10-02 18:50:30 +0200

    save

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 import Background from '../components/Background';
+import contactStyles from '../styles/Contact.module.scss';
 
 const contact = () => {
-  return <Background></Background>;
+  return (
+    <Background>
+      <div className={contactStyles.container}>
+        <a target="blank" href="resume.pdf">
+          <button className={contactStyles.resume}>RESUME</button>
+        </a>
+        <div className={contactStyles.infos}>
+          <p className={contactStyles.info}>+32 472 52 18 75</p>
+          <p className={contactStyles.info}>Belgium</p>
+          <p className={contactStyles.info}>Killian.Frappart@hotmail.com</p>
+        </div>
+      </div>
+    </Background>
+  );
 };
 
 export default contact;
